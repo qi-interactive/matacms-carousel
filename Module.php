acfa19d0547ea180ebd94eec954d036a448d1146
Michal Fiedorowicz <michal@qi-interactive.com>
2015-03-10 17:22:58 +0000

    Update

--- Module.php
+++ Module.php
@@ -23,6 +23,7 @@ use mata\base\Module as BaseModule;
 class Module extends BaseModule {
 
 	public function getNavigation() {
+		
 		$carousels = \matacms\carousel\models\Carousel::find()->select(['Title', 'Region'])->asArray()->all();
 		return $carousels;
 	}
